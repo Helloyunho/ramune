42d76b1b9ed9e76e791d78a7de42c7db54202d09
Helloyunho <me@helloyunho.xyz>
2026-08-06 18:59:24 +0900

    feat: resize it back to original

--- commands/crispify.py
+++ commands/crispify.py
@@ -28,6 +28,39 @@ class Crispify(CogLogger):
             param.default = discord.utils.MISSING
 
     async def crispify_media(self, url: str, target_format: str) -> bytes | str:
+        size = None
+        if target_format in ["mp4", "png"]:
+            cmd = [
+                "ffprobe",
+                "-v",
+                "error",
+                "-select_streams",
+                "v:0",
+                "-show_entries",
+                "stream=width,height",
+                "-of",
+                "csv=s=x:p=0",
+                url,
+            ]
+            process = await asyncio.create_subprocess_exec(
+                *cmd,
+                stdout=asyncio.subprocess.PIPE,
+                stderr=asyncio.subprocess.PIPE,
+            )
+            stdout, stderr = await process.communicate()
+
+            if process.returncode != 0:
+                self.logger.error(f"FFprobe error: {stderr.decode()}")
+                return stderr.decode()
+            size = stdout.decode().strip().split("x")
+            if len(size) != 2:
+                self.logger.error(
+                    f"FFprobe error: unexpected output: {stdout.decode()}"
+                )
+                return f"Unexpected FFprobe output: {stdout.decode()}"
+            size = [int(dim) for dim in size]
+            self.logger.debug(f"Original media size: {size[0]}x{size[1]}")
+
         cmd = [
             f"ffmpeg",
             "-i",
@@ -59,6 +92,38 @@ class Crispify(CogLogger):
         if process.returncode != 0:
             self.logger.error(f"FFmpeg error: {stderr.decode()}")
             return stderr.decode()
+
+        if target_format in ["mp4", "png"] and size:
+            self.logger.debug(f"Resizing back to {size[0]}x{size[1]}")
+            resize_cmd = [
+                "ffmpeg",
+                "-i",
+                "-",
+                "-loglevel",
+                "error",
+                "-vf",
+                f"scale={size[0]}:{size[1]}",
+                "-f",
+                target_format if target_format != "png" else "image2pipe",
+                *(
+                    ["-c:v", "png", "-f", "image2pipe"]
+                    if target_format == "png"
+                    else []
+                ),
+                "-",
+            ]
+            resize_process = await asyncio.create_subprocess_exec(
+                *resize_cmd,
+                stdin=asyncio.subprocess.PIPE,
+                stdout=asyncio.subprocess.PIPE,
+                stderr=asyncio.subprocess.PIPE,
+            )
+            stdout, stderr = await resize_process.communicate(input=stdout)
+
+            if resize_process.returncode != 0:
+                self.logger.error(f"FFmpeg resize error: {stderr.decode()}")
+                return stderr.decode()
+
         return stdout
 
     @commands.hybrid_command(
